578c880a44b8fa3fa777984328cd5e0b1f95c008
Gerard Pelly <gerardwp@gmail.com>
2019-10-26 11:38:47 +1100

    Updates

---
+++ precache-manifest.ca0b0bc58e6c7926680ce1e7dfada2f5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710d8ac5bc154f6ffd6b7d535a06f7aa",
+    "url": "/click/index.html"
+  },
+  {
+    "revision": "47926f1c5f87769a95d3",
+    "url": "/click/static/css/main.f1023598.chunk.css"
+  },
+  {
+    "revision": "6b9c672e16c6e00c117f",
+    "url": "/click/static/js/2.32c6e9bd.chunk.js"
+  },
+  {
+    "revision": "47926f1c5f87769a95d3",
+    "url": "/click/static/js/main.101c9e7a.chunk.js"
+  },
+  {
+    "revision": "193272e3087f7e39f3f6",
+    "url": "/click/static/js/runtime-main.01f8abd8.js"
+  },
+  {
+    "revision": "63c4b5f6ba4a9d698496b4fd7f8da91a",
+    "url": "/click/static/media/aliens.63c4b5f6.jpg"
+  },
+  {
+    "revision": "d318f380dae18fe80eed6a9cffe12ff9",
+    "url": "/click/static/media/batman.d318f380.jpg"
+  },
+  {
+    "revision": "eddb5e2fdf0a29a8f0a4cd3fd9374ea9",
+    "url": "/click/static/media/crying.eddb5e2f.jpg"
+  },
+  {
+    "revision": "4c76e9f491ad4d295d704e46b1330b98",
+    "url": "/click/static/media/doge.4c76e9f4.jpg"
+  },
+  {
+    "revision": "d3526ba2a0067e83bfa33f6a11059bc7",
+    "url": "/click/static/media/fry.d3526ba2.jpg"
+  },
+  {
+    "revision": "7dcdcec7da8ba11f37f51e86167b7e7e",
+    "url": "/click/static/media/genius.7dcdcec7.jpg"
+  },
+  {
+    "revision": "3dc6eb4249028de423b971c6e4dc6054",
+    "url": "/click/static/media/grumpy.3dc6eb42.jpg"
+  },
+  {
+    "revision": "e5c203cf1280b5b9ca4d155396d281b3",
+    "url": "/click/static/media/high.e5c203cf.jpg"
+  },
+  {
+    "revision": "8ff11eac395078c29fdac749ae50243d",
+    "url": "/click/static/media/karen.8ff11eac.jpg"
+  },
+  {
+    "revision": "6888c2e087c69a2cad1f28a96c800074",
+    "url": "/click/static/media/man.6888c2e0.jpg"
+  },
+  {
+    "revision": "16a78e9ff6275bef46c9db9a36d0ca23",
+    "url": "/click/static/media/meme-bg.16a78e9f.jpg"
+  },
+  {
+    "revision": "9371af2d32e52968ad168966280d1d70",
+    "url": "/click/static/media/meme-duck.9371af2d.png"
+  },
+  {
+    "revision": "e19a07ebc5e424b54ffd529d28aba115",
+    "url": "/click/static/media/nerd.e19a07eb.jpg"
+  },
+  {
+    "revision": "cedfcef44a16ad34a9d1de65c8894c7a",
+    "url": "/click/static/media/success.cedfcef4.png"
+  },
+  {
+    "revision": "6592af549fb54bad049da5ffc46c8387",
+    "url": "/click/static/media/yoda.6592af54.jpg"
+  }
+]);
